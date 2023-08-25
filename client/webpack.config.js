fd8192f73d9544f5eaec913ddfbbfcd59d0e593b
Joshua Hebert <jhebertcocktails@gmail.com>
2023-08-24 20:43:27 -0400

    added webpack and configured it for a basic setup

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,10 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
+const { InjectManifest } = require('workbox-webpack-plugin');
 
+//  Add and configure workbox plugins for a service worker and manifest file.
+// Add CSS loaders and babel to webpack.
 module.exports = () => {
   return {
     mode: 'development',
@@ -8,20 +13,64 @@ module.exports = () => {
       install: './src/js/install.js',
       cards: './src/js/cards.js'
     },
-
-    // TODO: Add the correct output
     output: {
-      
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
     },
-
-    // TODO: Add the correct plugins
     plugins: [
-     
+      // Webpack plugin that generates an HTML file for your application
+      // by injecting automatically all your generated bundles.
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin'
+      }),
+      // Injects bundles into the HTML file.
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'service-worker.js',
+      }),
+      // Webpack plugin that generates a manifest.json file
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'A text editor for online and offline use',
+        background_color: '#7eb4e2',
+        theme_color: '#7eb4e2',
+        start_url: '/',
+        public_path: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
-
-    // TODO: Add the correct modules
     module: {
-
-    }
+      // CSS loader for.js files
+      rules: [
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          // Use babel-loader to transpile JavaScript files.
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        }
+      ],
+    },
   };
 };
+
+
