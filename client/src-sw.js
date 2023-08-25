18d58d0afe9cbe6121d901a776f9df0e090a17f0
Joshua Hebert <jhebertcocktails@gmail.com>
2023-08-24 20:41:14 -0400

    added package.json and service worker

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,1 +1,45 @@
-// TODO: Create a service worker that caches static assets:
+const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
+const { CacheFirst } = require('workbox-strategies');
+const { registerRoute } = require('workbox-routing');
+const { CacheableResponsePlugin } = require('workbox-cacheable-response');
+const { ExpirationPlugin } = require('workbox-expiration');
+const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+
+precacheAndRoute(self.__WB_MANIFEST);
+
+const pageCache = new CacheFirst({
+    cacheName: 'page-cache',
+    plugins: [
+        new CacheableResponsePlugin({
+            statuses: [0, 200],
+        }),
+        new ExpirationPlugin({
+            maxAgeSeconds: 30 * 24 * 60 * 60,
+        }),
+    ],
+});
+
+warmStrategyCache({
+    urls: ['/index.html', '/'],
+    strategy: pageCache,
+});
+
+registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+// Implement asset caching
+registerRoute(({ request }) =>
+    // Create a callback function that will filter out any requests we want to cache
+    ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+    new StaleWhileRevalidate({
+        // Name of the cache Storage.
+        cacheName: 'assets-cache',
+        plugins: [
+            // Plugin will cache responses for 30 days.
+            new CacheableResponsePlugin({
+                statuses: [0, 200],
+            }),
+            // Plugin will delete old responses to conserve disk space.
+            new ExpirationPlugin({
+                maxAgeSeconds: 30 * 24 * 60 * 60,
+            }),
+        ],
+    }));
